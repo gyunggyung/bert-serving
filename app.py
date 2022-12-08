529bc223769453dd490e890122bff6ef31ef216c
gyunggyung <newhiwoong@gmail.com>
2022-12-08 10:17:09 +0000

    Add Caching

--- app.py
+++ app.py
@@ -2,19 +2,25 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text
 from flask import Flask, request, render_template
+from cachelib  import SimpleCache
 
-# Load required models
 encoder = hub.KerasLayer("https://tfhub.dev/jeongukjae/distilkobert_sentence_encoder/1")
 preprocessor = hub.KerasLayer("https://tfhub.dev/jeongukjae/distilkobert_cased_preprocess/1")
 
-# Define sentence encoder model
 inputs = tf.keras.Input([], dtype=tf.string)
 encoder_inputs = preprocessor(inputs)
 sentence_embedding = encoder(encoder_inputs)
 normalized_sentence_embedding = tf.nn.l2_normalize(sentence_embedding, axis=-1)
 model = tf.keras.Model(inputs, normalized_sentence_embedding)
 
+cache = SimpleCache()
 app = Flask(__name__)
+
+def find_cache(key):
+    if cache.get(key) is None:
+        sentence = tf.constant([key])
+        cache.set(key, model(sentence), timeout=3000)
+    return cache.get(key)
 
 @app.route('/')
 def home():
@@ -22,10 +28,10 @@ def home():
 
 @app.route('/classify', methods=['POST'])
 def classify():
-    sentence1 = tf.constant([request.form["sentence1"]])
-    sentence2 = tf.constant([request.form["sentence2"]])
-    embeddings1 = model(sentence1)
-    embeddings2 = model(sentence2)
+    sentence1 = request.form["sentence1"]
+    sentence2 = request.form["sentence2"]
+    embeddings1 = find_cache(sentence1)
+    embeddings2 = find_cache(sentence2)
     score = tf.tensordot(embeddings1, embeddings2, axes=[1, 1]).numpy()[0][0]
     return "<h3>두 문장의 유사도는: {}%입니다!</h3".format(round(score * 100, 2))
 
